bef5926f15c34b3b2f0bc9b1e7f2d065192a798c
jverfr1 <jverfr1@gmail.com>
2016-12-07 10:57:59 +0100

    geolocation fixed

--- app/assets/javascripts/geolocation.js
+++ app/assets/javascripts/geolocation.js
@@ -1,14 +1,14 @@
-// navigator.geolocation.getCurrentPosition(onLocation, error, options);
-// var options = {
-// 	enableHighAccuracy:true,
-// };
+var coords;
+navigator.geolocation.getCurrentPosition(onLocation, error, options);
+var options = {
+	enableHighAccuracy:true,
+};
 
-// function onLocation(position) {
-// 	coords = position.coords.latitud+","+position.coords.longitude;
-// 	return coords;
-// }
+function onLocation(position) {
+	coords = position.coords.latitude+","+position.coords.longitude;
+}
 
-// function error(error) {
-// 	console.log("error");
-// 	return true;
-// }
+function error(error) {
+	console.log("error");
+	return true;
+}
