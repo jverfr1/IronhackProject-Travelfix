80a800a017decf0091a17be626c033206366623b
jverfr1 <jverfr1@gmail.com>
2016-12-08 15:17:27 +0100

    Countries added to db doing scrape

--- app/models/embassy.rb
+++ app/models/embassy.rb
@@ -1,2 +1,23 @@
 class Embassy < ApplicationRecord
+  def self.get_countries_list
+    mechanize = Mechanize.new
+
+    page = mechanize.get('http://www.embassy-worldwide.com/')
+
+    span = page.search('span')
+
+    array = []
+    span.each do |country|
+      array.push(country.child.to_s)
+    end
+
+    countries = []
+    array.map! do |country|
+      if country.length < 30 && country != "Homepage"
+        countries.push(country)
+      end
+    end
+    countries.slice!(-1)
+    countries
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Embassy.get_countries_list.each do |country|
+  Embassy.create(name: country)
+end
