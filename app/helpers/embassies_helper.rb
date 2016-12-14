443a1ce4ffa22967807a5ecda937d417395e20f0
jverfr1 <jverfr1@gmail.com>
2016-12-14 12:29:21 +0100

    price added to search results

--- app/helpers/embassies_helper.rb
+++ app/helpers/embassies_helper.rb
@@ -1,17 +1,12 @@
 module EmbassiesHelper
-  def print_hotels_links(hotel)
-    hotel
-  end
-  def booking_url
-    @bookin="http://booking.com"
-  end
+
   def stars(number)
     @star=[]
     number.to_i.times do
-      @star.push("<i class='fa fa-star' aria-hidden='true'></i>")
+      @star << "<i class='fa fa-star' aria-hidden='true'></i>"
     end
     (5-number.to_i).times do
-      @star.push("<i class='fa fa-star-o' aria-hidden='true'></i>")
+      @star << "<i class='fa fa-star-o' aria-hidden='true'></i>"
     end
     @star.join().html_safe
   end
--- app/models/embassy.rb
+++ app/models/embassy.rb
@@ -48,40 +48,48 @@ class Embassy < ApplicationRecord
   def self.hotel_search(destination, from, to)
 
     hotels_results = []
-    hotels =fill_form(destination, from, to).search('.h-listing')[0].search('.hotel-wrap').search('.description') 
+    hotels =fill_form(destination, from, to).search('.h-listing')[0].search('.hotel-wrap') 
     hotels.each do |hotel|
       hotels_results << {
-        name: get_name(hotel),
         last_reservation: last_reservation(hotel),
-        link: get_link(hotel),
-        address: get_address(hotel),
-        image: get_image(hotel),
-        stars: get_stars(hotel)
+        address:          get_address(hotel),
+        image:            get_image(hotel),
+        stars:            get_stars(hotel),
+        price:            get_price(hotel),
+        price:            get_price_description(hotel)
+        name:             get_name(hotel),
+        link:             get_link(hotel),
         }
     end
     hotels_results
 
   end
   def self.get_name(hotel)
-    hotel.search('h3').text.chomp
+    hotel.search('.description').search('h3').text.chomp
   end
   def self.last_reservation(hotel)
-    hotel.search('.messaging').text.chomp
+    hotel.search('.description').search('.messaging').text.chomp
   end
   def self.get_link(hotel)
-    "https://es.hoteles.com/"+hotel.search('h3').search('a')[0].attributes["href"].value
+    "https://es.hoteles.com/"+hotel.search('.description').search('h3').search('a')[0].attributes["href"].value
   end
   def self.get_address(hotel)
-    hotel.search('.contact').text.chomp
+    hotel.search('.description').search('.contact').text.chomp
   end
   def self.get_image(hotel)
-    hotel.search('.image-and-details')[0].search('.image')[0].children[0].search('img')[0].attributes['style'].value.split("'")[1].to_s  
+    hotel.search('.description').search('.image-and-details')[0].search('.image')[0].children[0].search('img')[0].attributes['style'].value.split("'")[1].to_s  
   end
   def self.format_date(date)
     Date.parse(date).strftime('%d/%m/%Y')
   end
   def self.get_stars(hotel)
-    hotel.search('.star-rating-container')[0].search('.widget-tooltip-bd').text.slice!(0)
+    hotel.search('.description').search('.star-rating-container')[0].search('.widget-tooltip-bd').text.slice!(0)
+  end
+  def self.get_price(hotel)
+    hotel.search('.pricing').search('.price')[0].text
+  end
+  def self.get_price_description(hotel)
+    hotel.search('.pricing').search('.price-breakdown').text
   end
   def self.check_date(date)
     date >= Date.today.strftime('%d %B,%Y')
