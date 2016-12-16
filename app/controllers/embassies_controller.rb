ec64fb323752d1e07d6a74bca05832590a149c9e
jverfr1 <jverfr1@gmail.com>
2016-12-16 04:23:30 +0100

    Problem in embassies controller fixed

--- app/controllers/embassies_controller.rb
+++ app/controllers/embassies_controller.rb
@@ -5,9 +5,6 @@ class EmbassiesController < ApplicationController
     @destination = params[:destination]
     @date1 = params[:from]
     @date2 = params[:to]
-    unless Embassy.check_date(@date1)
-      redirect_to '/'
-    end
     @hotels = Hotel.hotel_search(@destination,@date1,@date2).paginate(page: params[:page], per_page: 4)
   end
   def show
