3f0bb25dd891d52916472acfbe101e9ea41606a3
jverfr1 <jverfr1@gmail.com>
2016-12-16 03:20:04 +0100

    Tochins styles

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get '/', to: 'site#index'
   get '/', to: 'embassies#travel'
-  get '/travel/hotel_results', to: 'embassies#search', as: :search_results
-  get '/travel/embassy', to: 'embassies#show', as: :embassy_search
+  get '/hotel_results', to: 'embassies#search', as: :search_results
+  get '/embassies', to: 'embassies#show', as: :embassy_search
 end
